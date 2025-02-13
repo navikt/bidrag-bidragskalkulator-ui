737f1a8b4b2900a3b044909feaac84280a27e139
Kristofer Giltvedt Selbekk <kristofer.giltvedt.selbekk@nav.no>
2025-02-13 22:16:48 +0100

    Legg til linting

---
+++ types.d.ts
@@ -0,0 +1,17 @@
+declare module "eslint-plugin-react-hooks" {
+  import type { ESLint } from "eslint";
+  const plugin: ESLint.Plugin;
+  export default plugin;
+}
+
+declare module "eslint-plugin-react" {
+  import type { ESLint } from "eslint";
+  const plugin: ESLint.Plugin;
+  export default plugin;
+}
+
+declare module "@typescript-eslint/eslint-plugin" {
+  import type { ESLint } from "eslint";
+  const plugin: ESLint.Plugin;
+  export default plugin;
+}
