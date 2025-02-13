332b424ae3c7b96d5dc0f3871e4e928f4b11f0f2
Kristofer Giltvedt Selbekk <kristofer.giltvedt.selbekk@nav.no>
2025-02-13 15:25:20 +0100

    Legg til et guidepanel

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,4 +1,4 @@
-import { BodyShort, Page } from "@navikt/ds-react";
+import { BodyShort, GuidePanel, Page } from "@navikt/ds-react";
 import type { Route } from "./+types/home";
 
 export function meta({}: Route.MetaArgs) {
@@ -16,13 +16,25 @@ export default function Home() {
   return (
     <Page.Block>
       <h1>Bidragskalkulator</h1>
-      <div className="space-y">
-        <BodyShort>
-          Bidragskalkulatoren hjelper deg å regne ut hvor stort et barnebidrag
-          er.
-        </BodyShort>
-        <BodyShort>Tjenesten er under utvikling.</BodyShort>
-      </div>
+      <GuidePanel poster className="max-w-xl mx-auto">
+        <div className="space-y-4">
+          <BodyShort>
+            Bidragskalkulatoren er et verktøy som hjelper deg å beregne
+            barnebidrag. Barnebidrag er et månedlig beløp som skal dekke
+            utgifter til barnets daglige liv og oppvekst.
+          </BodyShort>
+          <BodyShort>
+            For å beregne bidraget trenger du informasjon om:
+          </BodyShort>
+          <ul className="list-disc list-inside space-y-2">
+            <li>Inntekten til begge foreldrene</li>
+            <li>Samværsordningen mellom foreldre og barn</li>
+            <li>Andre barn som bor fast hos en av foreldrene</li>
+            <li>Eventuelle særlige utgifter til barnet</li>
+          </ul>
+        </div>
+      </GuidePanel>
+      <BodyShort>Tjenesten er under utvikling.</BodyShort>
     </Page.Block>
   );
 }
