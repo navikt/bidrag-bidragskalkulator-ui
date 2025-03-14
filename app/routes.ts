736f824885c2d78ddb273c581be1a53991c3e974
Kristofer Giltvedt Selbekk <kristofer.giltvedt.selbekk@nav.no>
2025-03-14 09:12:51 +0100

    Flytt prodmiljøet til barnebidragskalkulator.nav.no

--- app/routes.ts
+++ app/routes.ts
@@ -1,7 +1,7 @@
-import { type RouteConfig, index, route } from "@react-router/dev/routes";
+import { type RouteConfig, route } from "@react-router/dev/routes";
 
 export default [
-  index("routes/home.tsx"),
+  route("/", "routes/home.tsx"),
   route("/api/internal/isalive", "routes/api/internal/isalive.ts"),
   route("/api/internal/isready", "routes/api/internal/isready.ts"),
 ] satisfies RouteConfig;
