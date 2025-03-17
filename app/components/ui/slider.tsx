a843cf8e835ea67280c538c4e729021afe6a57ad
Kristofer Giltvedt Selbekk <kristofer.giltvedt.selbekk@nav.no>
2025-03-17 12:57:15 +0100

    Fiks issue med slider som ikke fungerte som den skulle

--- app/components/ui/slider.tsx
+++ app/components/ui/slider.tsx
@@ -32,6 +32,7 @@ function Slider({
 }: SliderProps) {
   const generertId = useId();
   const id = eksternId ?? generertId;
+  const labelId = `${id}-label`;
   const verdi = value
     ? [Number(value)]
     : defaultValue
@@ -44,7 +45,9 @@ function Slider({
 
   return (
     <div className="flex flex-col gap-2">
-      <Label htmlFor={id}>{label}</Label>
+      <Label htmlFor={id} id={labelId}>
+        {label}
+      </Label>
       {description && (
         <BodyShort size="small" textColor="subtle">
           {description}
@@ -63,7 +66,6 @@ function Slider({
         onValueChange={handleChange}
         {...props}
         id={id}
-        aria-labelledby={id}
       >
         <SliderPrimitive.Track
           data-slot="slider-track"
@@ -77,6 +79,7 @@ function Slider({
           />
         </SliderPrimitive.Track>
         <SliderPrimitive.Thumb
+          aria-labelledby={labelId}
           data-slot="slider-thumb"
           className="border-primary bg-blue-500 ring-ring/50 block size-5 shrink-0 rounded-full border-none shadow-sm transition-[color,box-shadow] hover:ring-4 hover:ring-blue-500 focus-visible:ring-4 focus-visible:ring-blue-500 focus-visible:outline-hidden disabled:pointer-events-none disabled:opacity-50"
         />
