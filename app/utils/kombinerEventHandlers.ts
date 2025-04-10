c215fc8e396aeb9cbce893854a22c9255ea9af27
Kristofer Giltvedt Selbekk <kristofer.giltvedt.selbekk@nav.no>
2025-04-10 10:49:35 +0200

    Unngå tom pilfunksjon for CopyButton

--- app/utils/kombinerEventHandlers.ts
+++ app/utils/kombinerEventHandlers.ts
@@ -1,10 +1,10 @@
 type EventHandler<T> = (event: T) => void;
 
 export function kombinerEventHandlers<T>(
-  ...handlers: EventHandler<T>[]
+  ...handlers: (EventHandler<T> | undefined)[]
 ): EventHandler<T> {
   return (event: T) => {
-    handlers.forEach((handler) => {
+    handlers.filter(Boolean).forEach((handler) => {
       if (typeof handler === "function") {
         handler(event);
       }
