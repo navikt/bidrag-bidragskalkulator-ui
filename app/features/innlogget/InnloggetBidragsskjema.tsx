7f56e9d410dca096780b7a8ed5d48e61e5f3c4bb
Lisa Ottesen <lisa.kleppe.ottesen@nav.no>
2025-04-15 14:43:58 +0200

    Litt mer WIP

--- app/features/innlogget/InnloggetBidragsskjema.tsx
+++ app/features/innlogget/InnloggetBidragsskjema.tsx
@@ -2,21 +2,28 @@ import { Button } from "@navikt/ds-react";
 import { FormProvider, useForm } from "@rvf/react";
 import { definerTekster, useOversettelse } from "~/utils/i18n";
 import { usePersoninformasjon } from "./usePersoninformasjon";
-import { getInnloggetBidragsskjemaValidator } from "./schema";
-import { getInnloggetFormDefaultValues } from "./utils";
+import {
+  getInnloggetSkjema,
+  type InnloggetSkjema,
+  type InnloggetSkjemaValidert,
+} from "./schema";
 import { Motpart } from "./Motpart";
 import { BostedOgSamvær } from "./BostedOgSamvær";
 import { Inntektsopplysninger } from "./Inntektsopplysninger";
+import { getInnloggetFormDefaultValues } from "./utils";
 
 export function InnloggetBidragsskjema() {
   const personinformasjon = usePersoninformasjon();
   const { språk } = useOversettelse();
-  const validator = getInnloggetBidragsskjemaValidator(språk);
   const { t } = useOversettelse();
 
-  const form = useForm({
-    validator: validator,
+  const form = useForm<InnloggetSkjema, InnloggetSkjemaValidert, {}>({
+    schema: getInnloggetSkjema(språk),
+    submitSource: "state",
     defaultValues: getInnloggetFormDefaultValues(personinformasjon),
+    handleSubmit: (validatedValues) => {
+      console.log("Submitting form", validatedValues);
+    },
   });
 
   const harValgtMotpart = Boolean(form.value("barn"));
