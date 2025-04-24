9560a225c026f31dac092a20ce1a50f4083ce216
Kristofer Giltvedt Selbekk <kristofer.giltvedt.selbekk@nav.no>
2025-04-24 12:49:58 +0200

    Legg til parsing av public envs også

--- app/config/publicEnv.ts
+++ app/config/publicEnv.ts
@@ -1,5 +1,23 @@
+import { z } from "zod";
+
 const isServer = typeof process !== "undefined";
 const miljøvariabler = isServer ? process.env : import.meta.env;
+
+const envSchema = z.object({
+  UMAMI_WEBSITE_ID: z.string(),
+  ENVIRONMENT: z.enum(["dev", "prod"]),
+});
+
+const parsedEnv = envSchema.safeParse(miljøvariabler);
+if (!parsedEnv.success) {
+  console.error(
+    "Ugyldige miljøvariabler definert i Vite-configen",
+    parsedEnv.error
+  );
+  throw new Error(
+    "Ugyldige miljøvariabler: " + parsedEnv.error.format().toString()
+  );
+}
 
 /**
  * Offentlig tilgjengelige miljøvariabler.
@@ -7,6 +25,6 @@ const miljøvariabler = isServer ? process.env : import.meta.env;
  * Disse er tilgjengelige på klientsiden, og skal ikke inneholde sensitiv informasjon.
  */
 export const publicEnv = {
-  UMAMI_WEBSITE_ID: miljøvariabler.UMAMI_WEBSITE_ID,
-  ENVIRONMENT: miljøvariabler.ENVIRONMENT,
+  UMAMI_WEBSITE_ID: parsedEnv.data.UMAMI_WEBSITE_ID,
+  ENVIRONMENT: parsedEnv.data.ENVIRONMENT,
 };
