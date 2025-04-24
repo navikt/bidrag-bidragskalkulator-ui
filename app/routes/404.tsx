c6c5b5fb5d9c116ed0545455a704e712d7c20daa
Kristofer Giltvedt Selbekk <kristofer.giltvedt.selbekk@nav.no>
2025-04-24 12:52:01 +0200

    Returner riktig statuskode

--- app/routes/404.tsx
+++ app/routes/404.tsx
@@ -1,4 +1,9 @@
+import { data } from "react-router";
 import { NotFound } from "../features/feilhåndtering/404";
+
+export const loader = () => {
+  return data({}, { status: 404 });
+};
 
 export default function NotFoundRoute() {
   return <NotFound />;
