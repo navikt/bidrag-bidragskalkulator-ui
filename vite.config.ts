3c2918d70fc475fa5f57f55da3e6d472b83e9ab6
Kristofer Giltvedt Selbekk <kristofer.giltvedt.selbekk@nav.no>
2025-04-25 10:35:17 +0200

    Fjern ubrukt variabel

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { reactRouterDevTools } from "react-router-devtools";
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
   return {
     plugins: [
       tailwindcss(),
