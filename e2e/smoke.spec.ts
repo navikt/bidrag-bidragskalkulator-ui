2c51da8017c5f601d57ac25eea3ca5abcbea84e2
Kristofer Giltvedt Selbekk <kristofer.giltvedt.selbekk@nav.no>
2025-06-11 14:29:14 +0200

    Refaktorer ut et verktøy for å sjekke tilgjengelighet

--- e2e/smoke.spec.ts
+++ e2e/smoke.spec.ts
@@ -1,22 +1,11 @@
-import AxeBuilder from "@axe-core/playwright";
-import { expect, test } from "@playwright/test";
+import { test } from "@playwright/test";
+import { sjekkTilgjengelighet } from "./uu-utils";
 
 test.describe("Smoke test", () => {
   test("Det initielle bildet er tilgjengelig utformet", async ({ page }) => {
     await page.goto("/");
     await page.waitForSelector("body", { timeout: 10000 });
 
-    const tilgjengelighetsresultater = await new AxeBuilder({ page })
-      .include("#maincontent")
-      .analyze();
-
-    if (tilgjengelighetsresultater.violations.length > 0) {
-      console.error("🚫♿️ Fant UU-feil ♿️🚫");
-      console.error(
-        JSON.stringify(tilgjengelighetsresultater.violations, null, 2),
-      );
-    }
-
-    expect(tilgjengelighetsresultater.violations).toEqual([]);
+    await sjekkTilgjengelighet(page);
   });
 });
