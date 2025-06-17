5075cb467c966a0468c2457d1a884b4e3ad9e071
Kristofer Giltvedt Selbekk <kristofer.giltvedt.selbekk@nav.no>
2025-06-17 09:54:42 +0200

    Lag et nytt inputfelt for fødselsnumre

---
+++ app/features/skjema/FødselsnummerTextField.tsx
@@ -0,0 +1,53 @@
+import { type TextFieldProps } from "@navikt/ds-react";
+import { type Ref } from "react";
+import { InternalFormatterbartTextField } from "./InternalFormatterbartTextField";
+
+export type FødselsnummerTextFieldProps = Omit<TextFieldProps, "onChange"> & {
+  onChange: (value: string) => void;
+  ref?: Ref<HTMLInputElement | null>;
+};
+
+/**
+ * FødselsnummerTextField er en komponent som formaterer et tall som fødselsnummer.
+ * onChange-callbacken blir kalt med den uformatterte verdien.
+ *
+ * Ellers fungerer det som et vanlig TextField.
+ *
+ * ```tsx
+ * <FødselsnummerTextField
+ *   {...form.field("deg.fødselsnummer").getControlProps()}
+ *   label="Hva er fødselsnummeret ditt?"
+ * />
+ * ```
+ */
+export const FødselsnummerTextField = (props: FødselsnummerTextFieldProps) => {
+  return (
+    <InternalFormatterbartTextField
+      {...props}
+      formatterer={formatterTall}
+      avformatterer={avformatterTall}
+    />
+  );
+};
+
+FødselsnummerTextField.displayName = "FødselsnummerTextField";
+
+const formatterTall = (verdi: string | number | undefined): string => {
+  if (!verdi) return "";
+
+  const renVerdi = String(verdi).replace(/\D/g, "");
+
+  const begrensetVerdi = renVerdi.slice(0, 11);
+
+  if (begrensetVerdi.length <= 6) {
+    return begrensetVerdi;
+  } else {
+    const første6 = begrensetVerdi.slice(0, 6);
+    const siste5 = begrensetVerdi.slice(6);
+    return `${første6} ${siste5}`;
+  }
+};
+
+const avformatterTall = (verdi: string): string => {
+  return verdi.replace(/\s/g, "");
+};
