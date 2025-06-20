37617ead4ad912a23bb51d62763b2ee5c166f540
Kristofer Giltvedt Selbekk <kristofer.giltvedt.selbekk@nav.no>
2025-06-20 14:03:29 +0200

    Dedupliser mock-data

--- e2e/mocks/data.ts
+++ e2e/mocks/data.ts
@@ -7,6 +7,87 @@ import type {
   Personinformasjon,
 } from "~/features/skjema/personinformasjon/schema";
 
+const underholdskostnader = {
+  "0": 8313,
+  "1": 6547,
+  "2": 6547,
+  "3": 6547,
+  "4": 6547,
+  "5": 6547,
+  "6": 6547,
+  "7": 8471,
+  "8": 8471,
+  "9": 8471,
+  "10": 8471,
+  "11": 8471,
+  "12": 9673,
+  "13": 9673,
+  "14": 9673,
+  "15": 9673,
+  "16": 10778,
+  "17": 10778,
+  "18": 12288,
+  "19": 12288,
+  "20": 12288,
+  "21": 12288,
+  "22": 12288,
+  "23": 12288,
+  "24": 12288,
+  "25": 12288,
+};
+const samværsfradrag = [
+  {
+    alderFra: 0,
+    alderTil: 5,
+    beløpFradrag: {
+      SAMVÆRSKLASSE_1: 317,
+      SAMVÆRSKLASSE_2: 1048,
+      SAMVÆRSKLASSE_3: 2847,
+      SAMVÆRSKLASSE_4: 3574,
+    },
+  },
+  {
+    alderFra: 6,
+    alderTil: 10,
+    beløpFradrag: {
+      SAMVÆRSKLASSE_1: 443,
+      SAMVÆRSKLASSE_2: 1467,
+      SAMVÆRSKLASSE_3: 3432,
+      SAMVÆRSKLASSE_4: 4308,
+    },
+  },
+  {
+    alderFra: 11,
+    alderTil: 14,
+    beløpFradrag: {
+      SAMVÆRSKLASSE_1: 547,
+      SAMVÆRSKLASSE_2: 1813,
+      SAMVÆRSKLASSE_3: 3914,
+      SAMVÆRSKLASSE_4: 4913,
+    },
+  },
+  {
+    alderFra: 15,
+    alderTil: 18,
+    beløpFradrag: {
+      SAMVÆRSKLASSE_1: 623,
+      SAMVÆRSKLASSE_2: 2063,
+      SAMVÆRSKLASSE_3: 4262,
+      SAMVÆRSKLASSE_4: 5351,
+    },
+  },
+  {
+    alderFra: 19,
+    alderTil: 99,
+    beløpFradrag: {
+      SAMVÆRSKLASSE_1: 623,
+      SAMVÆRSKLASSE_2: 2063,
+      SAMVÆRSKLASSE_3: 4262,
+      SAMVÆRSKLASSE_4: 5351,
+    },
+  },
+];
+
 /**
  * Default mock data for personinformasjon endepunkt
  */
@@ -16,86 +97,8 @@ export const defaultPersoninformasjon: Personinformasjon = {
   },
   inntekt: 500000,
   barnerelasjoner: [],
-  underholdskostnader: {
-    "0": 8313,
-    "1": 6547,
-    "2": 6547,
-    "3": 6547,
-    "4": 6547,
-    "5": 6547,
-    "6": 6547,
-    "7": 8471,
-    "8": 8471,
-    "9": 8471,
-    "10": 8471,
-    "11": 8471,
-    "12": 9673,
-    "13": 9673,
-    "14": 9673,
-    "15": 9673,
-    "16": 10778,
-    "17": 10778,
-    "18": 12288,
-    "19": 12288,
-    "20": 12288,
-    "21": 12288,
-    "22": 12288,
-    "23": 12288,
-    "24": 12288,
-    "25": 12288,
-  },
-  samværsfradrag: [
-    {
-      alderFra: 0,
-      alderTil: 5,
-      beløpFradrag: {
-        SAMVÆRSKLASSE_1: 317,
-        SAMVÆRSKLASSE_2: 1048,
-        SAMVÆRSKLASSE_3: 2847,
-        SAMVÆRSKLASSE_4: 3574,
-      },
-    },
-    {
-      alderFra: 6,
-      alderTil: 10,
-      beløpFradrag: {
-        SAMVÆRSKLASSE_1: 443,
-        SAMVÆRSKLASSE_2: 1467,
-        SAMVÆRSKLASSE_3: 3432,
-        SAMVÆRSKLASSE_4: 4308,
-      },
-    },
-    {
-      alderFra: 11,
-      alderTil: 14,
-      beløpFradrag: {
-        SAMVÆRSKLASSE_1: 547,
-        SAMVÆRSKLASSE_2: 1813,
-        SAMVÆRSKLASSE_3: 3914,
-        SAMVÆRSKLASSE_4: 4913,
-      },
-    },
-    {
-      alderFra: 15,
-      alderTil: 18,
-      beløpFradrag: {
-        SAMVÆRSKLASSE_1: 623,
-        SAMVÆRSKLASSE_2: 2063,
-        SAMVÆRSKLASSE_3: 4262,
-        SAMVÆRSKLASSE_4: 5351,
-      },
-    },
-    {
-      alderFra: 19,
-      alderTil: 99,
-      beløpFradrag: {
-        SAMVÆRSKLASSE_1: 623,
-        SAMVÆRSKLASSE_2: 2063,
-        SAMVÆRSKLASSE_3: 4262,
-        SAMVÆRSKLASSE_4: 5351,
-      },
-    },
-  ],
+  underholdskostnader,
+  samværsfradrag,
 };
 
 /**
@@ -106,86 +109,8 @@ export const defaultManuellPersoninformasjon: ManuellPersoninformasjon = {
     ident: "12345678901",
   },
   inntekt: 500000,
-  underholdskostnader: {
-    "0": 8313,
-    "1": 6547,
-    "2": 6547,
-    "3": 6547,
-    "4": 6547,
-    "5": 6547,
-    "6": 6547,
-    "7": 8471,
-    "8": 8471,
-    "9": 8471,
-    "10": 8471,
-    "11": 8471,
-    "12": 9673,
-    "13": 9673,
-    "14": 9673,
-    "15": 9673,
-    "16": 10778,
-    "17": 10778,
-    "18": 12288,
-    "19": 12288,
-    "20": 12288,
-    "21": 12288,
-    "22": 12288,
-    "23": 12288,
-    "24": 12288,
-    "25": 12288,
-  },
-  samværsfradrag: [
-    {
-      alderFra: 0,
-      alderTil: 5,
-      beløpFradrag: {
-        SAMVÆRSKLASSE_1: 317,
-        SAMVÆRSKLASSE_2: 1048,
-        SAMVÆRSKLASSE_3: 2847,
-        SAMVÆRSKLASSE_4: 3574,
-      },
-    },
-    {
-      alderFra: 6,
-      alderTil: 10,
-      beløpFradrag: {
-        SAMVÆRSKLASSE_1: 443,
-        SAMVÆRSKLASSE_2: 1467,
-        SAMVÆRSKLASSE_3: 3432,
-        SAMVÆRSKLASSE_4: 4308,
-      },
-    },
-    {
-      alderFra: 11,
-      alderTil: 14,
-      beløpFradrag: {
-        SAMVÆRSKLASSE_1: 547,
-        SAMVÆRSKLASSE_2: 1813,
-        SAMVÆRSKLASSE_3: 3914,
-        SAMVÆRSKLASSE_4: 4913,
-      },
-    },
-    {
-      alderFra: 15,
-      alderTil: 18,
-      beløpFradrag: {
-        SAMVÆRSKLASSE_1: 623,
-        SAMVÆRSKLASSE_2: 2063,
-        SAMVÆRSKLASSE_3: 4262,
-        SAMVÆRSKLASSE_4: 5351,
-      },
-    },
-    {
-      alderFra: 19,
-      alderTil: 99,
-      beløpFradrag: {
-        SAMVÆRSKLASSE_1: 623,
-        SAMVÆRSKLASSE_2: 2063,
-        SAMVÆRSKLASSE_3: 4262,
-        SAMVÆRSKLASSE_4: 5351,
-      },
-    },
-  ],
+  underholdskostnader,
+  samværsfradrag,
 };
 
 /**
