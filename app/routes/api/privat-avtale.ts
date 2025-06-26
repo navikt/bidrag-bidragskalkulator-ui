61a266895e6647efbcfde0f7552df6b46922cc63
Lisa Ottesen <lisa.kleppe.ottesen@nav.no>
2025-06-26 15:04:25 +0200

    Fjerne unødvendige header-endringer

--- app/routes/api/privat-avtale.ts
+++ app/routes/api/privat-avtale.ts
@@ -3,18 +3,5 @@ import { medToken } from "~/features/autentisering/api.server";
 import { hentPrivatAvtaledokument } from "~/features/privatAvtale/api.server";
 
 export async function action({ request }: ActionFunctionArgs) {
-  const originalRespons = await medToken(request, hentPrivatAvtaledokument);
-
-  const nyHeaders = new Headers(originalRespons.headers);
-  nyHeaders.set(
-    "Content-Disposition",
-    "attachment; filename=privat-avtale.pdf",
-  );
-
-  const newRes = new Response(await originalRespons.blob(), {
-    status: originalRespons.status,
-    statusText: originalRespons.statusText,
-    headers: nyHeaders,
-  });
-  return newRes;
+  return medToken(request, hentPrivatAvtaledokument);
 }
