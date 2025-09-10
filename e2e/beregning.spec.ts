71614f925145ac19cad52bb3bf05b23888b7d090
Kristofer Giltvedt Selbekk <kristofer.giltvedt.selbekk@nav.no>
2025-09-10 14:56:53 +0200

    Forbedre smoketesten

--- e2e/beregning.spec.ts
+++ e2e/beregning.spec.ts
@@ -9,7 +9,6 @@ test.describe("Beregningstest", () => {
     await sjekkTilgjengelighet(page);
 
     await page.getByLabel("Hvor gammelt er barnet?").fill("4");
-    await page.getByLabel("Hvor gammelt er barnet?").blur();
     await page
       .getByLabel("Vi har avtale om fast bosted hos begge (delt fast bosted)")
       .check();
--- e2e/smoke.spec.ts
+++ e2e/smoke.spec.ts
@@ -2,9 +2,13 @@ import { test } from "@playwright/test";
 import { sjekkTilgjengelighet } from "./uu-utils";
 
 test.describe("Smoke test", () => {
-  test("Det initielle bildet er tilgjengelig utformet", async ({ page }) => {
-    await page.goto("/barnebidrag/tjenester/kalkulator");
+  test("Det fungerer å gå til kalkulatoren", async ({ page }) => {
+    await page.goto("/barnebidrag/tjenester/");
+
     await page.waitForSelector("body", { timeout: 10000 });
+
+    await sjekkTilgjengelighet(page);
+    await page.getByRole("link", { name: "Prøv den nye kalkulatoren" }).click();
 
     await sjekkTilgjengelighet(page);
   });
